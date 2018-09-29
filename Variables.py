b7a4392780a29c7fcebafd1d7408fd802ba908f0
HaniMYRadaideh <43704442+HaniMYRadaideh@users.noreply.github.com>
2018-09-29 23:25:03 +0800

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,17 @@
+
+
+def main():
+   Amount=30 #amount of time spend on working
+   You=35
+   data="{} is high or {} is low".format(Amount, You) # Format my data
+   print(data,type(data))
+   datalen="is good"
+   print(len(datalen))
+   print(datalen[3])
+
+
+
+
+
+
+if __name__ == '__main__': main()
